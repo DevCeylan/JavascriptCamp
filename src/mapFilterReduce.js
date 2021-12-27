ca3f73bab56f1092e0f4fca5ee03547cedd19fb1
Mehmet CEYLAN <ceylarino@gmail.com>
2021-12-27 11:47:52 +0300

    add notes about map-filter-reduce

--- src/mapFilterReduce.js
+++ src/mapFilterReduce.js
@@ -1,45 +1,54 @@
 let cart = [
-    {id:1, productName:"Telefon", quantity:3, unitPrice:4000},
-    {id:2, productName:"Bardak", quantity:2, unitPrice:30},
-    {id:3, productName:"Kalem", quantity:1, unitPrice:20},
-    {id:4, productName:"Şarj Cihazı", quantity:2, unitPrice:100},
-    {id:5, productName:"Kitap", quantity:3, unitPrice:30},
-    {id:6, productName:"Pot", quantity:5, unitPrice:150},
-]
+  { id: 1, productName: "Telefon", quantity: 3, unitPrice: 4000 },
+  { id: 2, productName: "Bardak", quantity: 2, unitPrice: 30 },
+  { id: 3, productName: "Kalem", quantity: 1, unitPrice: 20 },
+  { id: 4, productName: "Şarj Cihazı", quantity: 2, unitPrice: 100 },
+  { id: 5, productName: "Kitap", quantity: 3, unitPrice: 30 },
+  { id: 6, productName: "Pot", quantity: 5, unitPrice: 150 },
+];
 
-console.log("<ul>")
-cart.map(product=>{
-    console.log("<li>"+product.productName + " : " 
-    + product.unitPrice * product.quantity+"</li>")
-})
-console.log("</ul>")
+console.log("<ul>");
+cart.map((product) => {
+  console.log(
+    "<li>" +
+      product.productName +
+      " : " +
+      product.unitPrice * product.quantity +
+      "</li>"
+  );
+});
+console.log("</ul>");
 
-let total = cart.reduce((acc,product)=>acc+ product.unitPrice * product.quantity,0)
+let total = cart.reduce(
+  (acc, product) => acc + product.unitPrice * product.quantity,
+  0
+);
 
-//23.00 Dersteyiz
-console.log(total)
+console.log(total);
 
+let quantityOver2 = cart.filter((product) => product.quantity > 2);
 
-let quantityOver2 = cart.filter(product=>product.quantity>2)
+console.log(quantityOver2);
 
-console.log(quantityOver2)
-
-function addToCart(sepet) {
-    sepet.push({id:7, productName:"Ruhsat", quantity:1, unitPrice:20})
-}
-
-
-
-addToCart(cart)
-
-console.log(cart)
-
-let sayi = 10
-
-function sayiTopla(number) {
-    number += 1
-}
-sayiTopla(sayi)
-console.log(sayi)
-
-//23.00 Dersteyiz
+/**
+ *
+ * .map(isim => {işlem})
+ *  map fonksiyonu verilen dizideki her elemanı tek tek gezer
+ *  üzerine geldikçe içerideki nesneleri verilen "isim" ile referanslar
+ *  dolaşırken de her elemana, belirtilen "işlem"i uygular.
+ *
+ * .filter(product => "belirlenen şart")
+ *  bi tık adı üstünde elemanlar içerisinde filtreleme yapmaya yarar
+ *  hedef özellikle yeni bir yapı oluşturmak.
+ *  yine tüm elemanlar gezilirken şartı sağlayanlar yeni bir array olarak döner
+ *  || ve && ile birleşik şartlar (sorgular) da yazılabilir.
+ *
+ * .reduce((acc,product)=>acc + product.unitPrice, 0 )
+ *  en sonda belirtilen değer ile acc başlatır
+ *  dizideki her elemanı product referans adı ile gezer
+ *  gezi sırasında belirtilen değerler ve işlemler ile acc güncellenir.
+ *  örn: sepette toplam fiyat yazdırma
+ *
+ *
+ *
+ */
